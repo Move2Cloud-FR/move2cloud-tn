c96ae9f693890123153cd5c8575aa5f77562162f
Abderrahmen ISSA <abderrahmen.issa@move2cloud.fr>
2026-06-11 22:23:09 +0200

    Add pages

--- server.js
+++ server.js
@@ -19,10 +19,12 @@ app.get("/", (req, res) => res.render("index", { currentPage: "home" }));
 app.get("/actualites", (req, res) => res.render("actualites", { currentPage: "actualites" }));
 app.get("/rejoindre", (req, res) => res.render("rejoindre", { currentPage: "rejoindre" }));
 
-// Routes SPA (sections de index.html)
-["/services", "/about", "/values", "/approach", "/contact"].forEach(route => {
-  app.get(route, (req, res) => res.render("index", { currentPage: route.slice(1) }));
-});
+// Routes pages dédiées
+app.get("/services",  (req, res) => res.render("services",  { currentPage: "services" }));
+app.get("/about",     (req, res) => res.render("about",     { currentPage: "about" }));
+app.get("/values",    (req, res) => res.render("values",    { currentPage: "values" }));
+app.get("/approach",  (req, res) => res.render("approach",  { currentPage: "approach" }));
+app.get("/contact",   (req, res) => res.render("contact",   { currentPage: "contact" }));
 
 // Pages offres d'emploi
 app.get("/nous-rejoindre/:slug", (req, res) => {
